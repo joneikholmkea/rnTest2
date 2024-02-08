eb2a2f05ef07976e28db32fbdd04d30df805a335
Jon Eikholm <joneikholm@hotmail.com>
2024-02-08 08:30:01 +0100

    f

--- App.js
+++ App.js
@@ -1,13 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 export default function App() {
+  const [text, setText] = useState('')
+  const [totalText, setTotalText]= useState('')
+  const [notes, setNotes] = useState([])
+
+  function addNotePressed(){
+    // setTotalText(totalText + text + "\n")
+    setText("")
+    setNotes([...notes, {key:notes.length, name:text}])
+  }
+
+  const saveNotes = async () => {
+    try {
+      const jsonValue = JSON.stringify(notes)
+      await AsyncStorage.setItem('@storage_Key', jsonValue)
+    } catch (e) { }
+  }
+  
+  const loadNotes = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem('@storage_Key')
+      if(jsonValue != null){
+        const arr =  JSON.parse(jsonValue);
+        if(arr!=null){
+          setNotes(arr)
+        }
+      }
+    } catch(e) {}
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{"< Notes >"}  </Text>
+      <TextInput style={styles.textInput} value={text} onChangeText={setText} />
+      <Button title="Add Note" onPress={addNotePressed}/>
+      <Text>{totalText}</Text>
+      <FlatList 
+        data={notes}
+        renderItem={(note) => <Text>{note.item.name}</Text>}
+      />
+      <Button title="Save Notes" onPress={saveNotes}/>
+      <Button title="Load Notes" onPress={loadNotes}/>
       <StatusBar style="auto" />
     </View>
-  );
+  );x
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +58,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textInput:{
+    backgroundColor:'lightblue',
+    minWidth: 200
+  }
 });
